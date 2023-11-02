01bab9e582efd3e6d2f6baeb764d60b5003f1883
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-02 15:44:42 +0100

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>hello</h1>
+		<div className='bg-darkBlue min-h-screen flex items-center justify-center'>
+			<h1 className='text-secondary'>hello</h1>
 		</div>
 	)
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		colors: {
+			darkBlue: '#1a2b33',
+			primary: '#31c6be',
+			secondary: '#f2b136',
+			gray: '#aabfca',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
