1767e51e61ee6a57cdd0ca7209f380e8b4b4549d
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-06 09:37:18 +0100

    add board state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import Square from './components/Square'
-/* import { IconO } from './icons/IconO'
-import { IconX } from './icons/IconX' */
+import { IconO } from './icons/IconO'
+import { IconX } from './icons/IconX'
 
 const TURNS = {
 	X: 'x',
 	O: 'o',
 }
 
+const initialState = Array(9).fill(null)
+
 function App() {
-	const [board, setBoard] = useState(Array(9).fill(null))
+	const [board, setBoard] = useState(initialState)
+
+	const updateBoard = (index) => {
+		const newBoard = board
+		newBoard[index] = 'x'
+		setBoard(newBoard)
+	}
+
+	console.log(board)
 
 	return (
 		<div className='bg-darkBlue min-h-screen flex items-center justify-center'>
 			<div className='grid grid-cols-3 gap-8'>
 				{board.map((sq, index) => (
-					<Square key={index}>
-						{/* <IconX width='7em' height='7em' color='#31c6be' /> */}
-						{/* <IconO width='7em' height='7em' color='#f2b136' /> */}
+					<Square key={index} onClick={() => updateBoard(index)}>
+						{board[index] === 'x' && (
+							<IconX width='7em' height='7em' color='#31c6be' />
+						)}
+						{board[index] === 'o' && (
+							<IconO width='7em' height='7em' color='#f2b136' />
+						)}
 					</Square>
 				))}
 			</div>
