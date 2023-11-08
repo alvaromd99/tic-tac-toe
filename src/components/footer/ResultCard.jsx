2b3fc9c93e0cfa4a42f0ab73f7e46aef9acb728c
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-08 11:09:13 +0100

    delete logs

--- src/components/footer/ResultCard.jsx
+++ src/components/footer/ResultCard.jsx
@@ -1,11 +1,14 @@
 export default function ResultCard({ index, resultCont }) {
+	const resultNames = ['X (YOU)', 'DRAW', 'O CPU']
+	const resultName = resultNames[index] || ''
+
 	const classNames = ['bg-primary', 'bg-gray', 'bg-secondary']
 	const className = classNames[index] || ''
 
 	return (
 		<div
-			className={`${className} w-40 h-20 flex flex-col items-center justify-center rounded-lg`}>
-			<p>You</p>
+			className={`${className} w-40 h-20 flex flex-col items-center justify-center rounded-lg shadow-2`}>
+			<p>{resultName}</p>
 			<span className='text-2xl font-bold'>{resultCont}</span>
 		</div>
 	)
