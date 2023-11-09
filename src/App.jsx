c57d9f9cc1b3aae8f43fa58c5faa0c3ace86d017
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-09 10:02:42 +0100

    add useBoard custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Board from './components/board/Board'
 import Footer from './components/footer/Footer'
-import { checkWinner, checkEndGame } from './components/logic/board'
-import { TURNS, initialResults, initialState } from './constants/constants'
+import { UseBoard } from './hooks/UseBoard'
 
 function App() {
-	const [board, setBoard] = useState(initialState)
-	const [winner, setWinner] = useState(null)
-
-	const [turn, setTurn] = useState(TURNS.X)
-	const [results, setResults] = useState(initialResults)
-
-	const updateBoard = (index) => {
-		if (board[index] || winner) return
-
-		const newBoard = [...board]
-		newBoard[index] = turn
-
-		setBoard(newBoard)
-		updateTurn(turn)
-
-		const newWinner = checkWinner(newBoard)
-		if (newWinner) {
-			setWinner(newWinner)
-			updateResults(newWinner)
-		} else if (checkEndGame(newBoard)) {
-			const draw = false
-			setWinner(draw)
-			updateResults(draw)
-		}
-	}
-
-	const resetGame = () => {
-		setBoard(initialState)
-		setWinner(null)
-	}
-
-	const updateTurn = (turn) => {
-		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-		setTurn(newTurn)
-	}
-
-	const updateResults = (winner) => {
-		setResults((prevResults) => ({
-			...prevResults,
-			X: winner === TURNS.X ? prevResults.X + 1 : prevResults.X,
-			O: winner === TURNS.O ? prevResults.O + 1 : prevResults.O,
-			D: winner === false ? prevResults.D + 1 : prevResults.D,
-		}))
-	}
+	const { turn, board, updateBoard, results, resetGame } = UseBoard()
 
 	return (
 		<div className='bg-darkBlue min-h-screen flex items-center justify-center'>
---
+++ src/hooks/UseBoard.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { TURNS, initialResults, initialState } from '../constants/constants'
+import { checkWinner, checkEndGame } from '../components/logic/board'
+
+export const UseBoard = () => {
+	const [board, setBoard] = useState(initialState)
+	const [winner, setWinner] = useState(null)
+
+	const [turn, setTurn] = useState(TURNS.X)
+	const [results, setResults] = useState(initialResults)
+
+	const updateBoard = (index) => {
+		if (board[index] || winner) return
+
+		const newBoard = [...board]
+		newBoard[index] = turn
+
+		setBoard(newBoard)
+		updateTurn(turn)
+
+		const newWinner = checkWinner(newBoard)
+		if (newWinner) {
+			setWinner(newWinner)
+			updateResults(newWinner)
+		} else if (checkEndGame(newBoard)) {
+			const draw = false
+			setWinner(draw)
+			updateResults(draw)
+		}
+	}
+
+	const resetGame = () => {
+		setBoard(initialState)
+		setWinner(null)
+	}
+
+	const updateTurn = (turn) => {
+		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+		setTurn(newTurn)
+	}
+
+	const updateResults = (winner) => {
+		setResults((prevResults) => ({
+			...prevResults,
+			X: winner === TURNS.X ? prevResults.X + 1 : prevResults.X,
+			O: winner === TURNS.O ? prevResults.O + 1 : prevResults.O,
+			D: winner === false ? prevResults.D + 1 : prevResults.D,
+		}))
+	}
+	return { board, updateBoard, winner, turn, results, resetGame }
+}
