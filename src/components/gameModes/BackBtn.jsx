956aa91faae72818d9595e34f6b86f647580c871
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-12 23:21:17 +0100

    add css to the back btn

--- src/components/gameModes/BackBtn.jsx
+++ src/components/gameModes/BackBtn.jsx
@@ -1,7 +1,12 @@
+import { ArrowLeft } from '../../icons/ArrowLeft'
+
 export const BackBtn = ({ changeGameMode }) => {
 	return (
-		<div onClick={() => changeGameMode(null)} className='absolute top-4 left-4'>
-			Back
+		<div
+			onClick={() => changeGameMode(null)}
+			className='absolute top-4 left-4 w-28 h-10 border-2 border-gray  flex items-center justify-evenly'>
+			<ArrowLeft width='2em' height='2em' color='#aabfca' />
+			<p className='font-bold text-gray'>Back</p>
 		</div>
 	)
 }
--- src/components/selector/MarkSelector.jsx
+++
@@ -1,3 +0,0 @@
-export const MarkSelector = () => {
-	return <div>MarkSelector</div>
-}
