1ca9025359d5d7d17a71ed354b27b85d2d4517a3
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-13 10:38:54 +0100

    add shadow

--- src/components/gameModes/BackBtn.jsx
+++ src/components/gameModes/BackBtn.jsx
@@ -7,7 +7,7 @@ export const BackBtn = () => {
 	return (
 		<div
 			onClick={() => changeGameMode(null)}
-			className='absolute top-4 left-4 w-28 h-10 border-2 border-gray  flex items-center justify-evenly'>
+			className='absolute top-4 left-4 w-28 h-10 border-2 border-gray  flex items-center justify-evenly shadow-2'>
 			<ArrowLeft width='2em' height='2em' color='#aabfca' />
 			<p className='font-bold text-gray'>Back</p>
 		</div>
